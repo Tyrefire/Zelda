19c2ade2ac4f2264c4766f2532816973fc83c220
Tyler Madsen <tylermadsen@mail.weber.edu>
2017-04-18 14:40:40 +0000

    upload

---
+++ Zelda_hw10_task1.c
@@ -0,0 +1,36 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  Zelda_hw10_task1.c
+ *       	Usage:  ./Zelda_hw10_task1.c
+ *    Description:  Which house is cheaper?
+ *
+ *        Version:  1.0
+ *        Created:  04/18/2017 02:38:13 PM
+ *       Compiler:  gcc -Wall -Werror
+ *         Author:  Tyler Madsen (), tylermadsen@mail.weber.edu
+ * =====================================================================================
+ */
+#include <stdio.h>		/* For Standard I/O */
+#include <stdlib.h>
+//#include <time.h>
+//#include <math.h>
+//#include <string.h>
+
+/* Local Libraries */
+
+
+/* Function Prototypes */
+
+
+/* Main Program */
+int main(int argc, char *argv[])
+{
+
+	return 0;
+}
+
+
+/* Function Definitions */
+
+
