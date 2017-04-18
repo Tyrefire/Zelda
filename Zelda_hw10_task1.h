f8e4e8d8a0f43c39fa8efd0dc116ed6742f5bfb3
Andrewlittle97 <andrewlittle@mail.weber.edu>
2017-04-18 15:54:05 +0000

    i

--- Zelda_hw10_task1.h
+++ Zelda_hw10_task1.h
@@ -28,20 +28,9 @@ struct house
 /* Function Prototypes */
 // 1) Function for InputHouse
 struct house InputHouse(void);
-	struct house Input
-{
-	printf("Enter the street number\n");
-	scanf("%lf", &Input.stNumber);
-	printf("Enter the street name\n");
-	scanf("%s", &Input.stName);
-	printf("Enter the price");
-	scanf("%lf", &Input.Price);
-
-	return Input;
-}
 // 2) Function to PrintHouse info
 void PrintHouse(char *name, struct house h);
 // 3) FUnction to Compare Prices
-int ComparePrices(..........);
+int ComparePrices(void);
 
 
