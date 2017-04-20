90e8369cdc528e25f3c8b7f4cd00e58aa7fbcfe5
Orlando Salas <orlandosalas@mail.weber.edu>
2017-04-19 18:41:56 -0600

    funcs 1 & 2 are done

--- Zelda_hw10_task1_funcs.c
+++ Zelda_hw10_task1_funcs.c
@@ -13,40 +13,55 @@
  */
 #include <stdio.h>		/* For Standard I/O */
 #include <stdlib.h>
+#include "Zelda_hw10_task1.h" 
 
 /* Function Defenitions */
-// 1) Function for InputHouse
+
 struct house InputHouse(void)
 {
 	
-	struct house Input[NUM];
-
-	for( i = 0; i < NUM; i++)
-	{
-	printf("Enter the street number\n");
-	scanf("%lf", &Input[NUM].stNumber);
-	printf("Enter the street name\n");
-	scanf("%s", &Input[NUM].stName);
-	printf("Enter the price");
-	scanf("%lf", &Input[NUM].Price);
-	}
+    struct house Input;
+    char c;
+    char catch;
+    int i = 0;
+    /* cap st # */
+	printf("Enter the street number: \n"); 
+	scanf("%d", &Input.stNumber);
+    scanf("%c", &catch); // catches enter key
+    /* cap st name */
+    printf("Enter the street name: \n");
+    while(i < (20 - 1) && ( c = getchar() ) != '\n')
+    {
+        Input.stName[i] = c ;
+        i++;
+    }
+    Input.stName[i] = '\0'; // finished your string
+    // printf("You entered [%s]\n", Input.stName);
+    /* cap price */
+    printf("Enter the price: \n");
+	scanf("%lf", &Input.Price);
+    //printf("Price: [%lf]\n", Input.Price);
 
 	return Input;
 }
-// 2) Function to PrintHouse info
+
 void PrintHouse(char *name, struct house h)
 {
-
+    printf("%s\n", name);
+    printf("STREET #: [%d]\n", h.stNumber);
+    printf("STREET NAME: [%s]\n", h.stName);
+    printf("PRICE: [%.2lf]\n", h.Price);
+   
 
 	return;
 }
 
 // 3) FUnction to Compare Prices
-int ComparePrices(void)
-{
+//int ComparePrices(void)
+//{
 
-	return;
-}
+//	return;
+//}
 
 
 
